316157b7b882232ba9d3d11680f6d3ce05918474
Federico Meloni <federico.meloni@cern.ch>
2023-10-16 21:30:48 +0200

    tau gun

---
+++ Gen/lcio_tau_gun_pt.py
@@ -0,0 +1,149 @@
+#####################################
+#
+# simple script to create lcio files with single particle
+# events - modify as needed
+# @author F.Gaede, DESY
+# @date 1/07/2014
+#
+# initialize environment:
+#  export PYTHONPATH=${LCIO}/src/python:${ROOTSYS}/lib
+#
+#####################################
+import math
+import random
+from array import array
+import ROOT
+from ROOT import TF1, TLorentzVector
+
+# --- LCIO dependencies ---
+from pyLCIO import UTIL, EVENT, IMPL, IO, IOIMPL
+
+# ---- number of events ----------------------
+nevt = 100000
+
+outfile = "tauGun_pT_0_50_gen.slcio"
+
+# --------------------------------------------
+
+
+wrt = IOIMPL.LCFactory.getInstance().createLCWriter()
+
+wrt.open(outfile, EVENT.LCIO.WRITE_NEW)
+
+random.seed()
+
+
+# ========== particle properties ===================
+
+# particles per event
+npart = 1
+
+genstat = 1
+
+pt_min = 0.5
+pt_max = 50.
+
+theta_min = 8./180.*math.pi
+theta_max = 172./180.*math.pi
+
+pdg = 15
+
+mass = 1.77686  # tau mass
+charge = -1.
+
+decayLen = 87.03e-6 # m
+#decayLen = 1.e22
+
+beamspot_sigma = 1.5  # mm
+
+# =================================================
+
+
+for j in range(0, nevt):
+
+    col = IMPL.LCCollectionVec(EVENT.LCIO.MCPARTICLE)
+    evt = IMPL.LCEventImpl()
+
+    evt.setEventNumber(j)
+
+    evt.addCollection(col, "MCParticle")
+
+    print(j, "-----------------------------")
+
+    for ipart in range(0, npart):
+
+        pt = random.uniform(pt_min, pt_max)
+        theta = random.uniform(theta_min, theta_max)
+        phi = random.random() * math.pi * 2.
+
+        p = pt/math.sin(theta)
+        energy = math.sqrt(mass*mass + p * p)
+
+        if energy > 5000:
+            continue
+
+        px = pt * math.cos(phi)
+        py = pt * math.sin(phi)
+        pz = p * math.cos(theta)
+
+        momentum = array('f', [px, py, pz])
+
+        # --- endpoint
+
+        myTau = TLorentzVector()
+        myTau.SetPxPyPzE(px,py,pz,energy)
+
+        denominator = decayLen * myTau.Gamma() * myTau.Beta()
+        exponential = TF1("myExp", "(1/[0])*TMath::Exp(-1*x/[0])")
+        exponential.SetParameter(0, denominator)
+        flight = exponential.GetRandom()
+
+        epx = flight * math.cos(phi) * math.sin(theta)
+        epy = flight * math.sin(phi) * math.sin(theta)
+        epz = flight * math.cos(theta)
+
+        endpoint = array('d', [epx, epy, epz])
+
+        # --- production vertex
+
+        vpx = 0.
+        vpy = 0.
+        vpz = random.gauss(0., beamspot_sigma)
+
+        vertex = array('d', [vpx, vpy, vpz])
+
+        time = 0.
+
+        # --- particle charge
+
+        if ipart % 2 == 1:
+            pdg = -pdg
+            charge = -charge
+
+
+# --------------- create MCParticle -------------------
+
+        mcp = IMPL.MCParticleImpl()
+
+        mcp.setGeneratorStatus(genstat)
+        mcp.setMass(mass)
+        mcp.setPDG(pdg)
+        mcp.setMomentum(momentum)
+        mcp.setCharge(charge)
+        mcp.setVertex(vertex)
+        mcp.setTime(time)
+
+        if (decayLen < 1.e9):   # arbitrary ...
+            mcp.setEndpoint(endpoint)
+
+        print("  ", ipart, pdg, charge, pt, phi, theta)
+
+
+# -------------------------------------------------------
+
+        col.addElement(mcp)
+
+    wrt.writeEvent(evt)
+
+
+wrt.close()
---
+++ Util/steer_pruneBIB_CONDOR.py
@@ -0,0 +1,46 @@
+from Configurables import ApplicationMgr
+from Gaudi.Configuration import *
+
+from Configurables import LcioEvent, EventDataSvc, MarlinProcessorWrapper
+from k4MarlinWrapper.parseConstants import *
+algList = []
+evtsvc = EventDataSvc()
+
+CONSTANTS = {
+}
+
+parseConstants(CONSTANTS)
+
+read = LcioEvent()
+read.OutputLevel = INFO
+read.Files = ["/data/BIB10TeV/sim_TYPEEVENT/BIB_sim_INFILENAME.slcio"]
+algList.append(read)
+
+EventNumber = MarlinProcessorWrapper("EventNumber")
+EventNumber.OutputLevel = INFO
+EventNumber.ProcessorType = "Statusmonitor"
+EventNumber.Parameters = {
+    "HowOften": ["1"]
+}
+
+Output_LCIO = MarlinProcessorWrapper("Output_LCIO")
+Output_LCIO.OutputLevel = INFO
+Output_LCIO.ProcessorType = "LCIOOutputProcessor"
+Output_LCIO.Parameters = {
+    "DropCollectionNames": ["MCParticle"],
+    "DropCollectionTypes": [],
+    "FullSubsetCollections": [],
+    "KeepCollectionNames": [],
+    "LCIOOutputFile": ["/data/BIB10TeV/sim_TYPEEVENT_pruned/BIB_sim_INFILENAME.slcio"],
+    "LCIOWriteMode": ["WRITE_NEW"]
+}
+
+algList.append(EventNumber)
+algList.append(Output_LCIO)
+
+ApplicationMgr(TopAlg=algList,
+               EvtSel='NONE',
+               EvtMax=-1,
+               ExtSvc=[evtsvc],
+               OutputLevel=INFO
+               )
