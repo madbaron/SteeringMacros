8389779b8b8da4d2a574e4ce5170efdd7055a81c
Federico Meloni <federico.meloni@cern.ch>
2025-09-07 21:59:16 +0200

    added BIB digi steer for calo composition studies

---
+++ k4Reco/steer_digi_for_BIB_composition.py
@@ -0,0 +1,356 @@
+from Configurables import ApplicationMgr
+from Gaudi.Configuration import *
+
+from Configurables import LcioEvent, EventDataSvc, MarlinProcessorWrapper
+from k4MarlinWrapper.parseConstants import *
+
+import os
+
+from k4FWCore.parseArgs import parser
+
+parser.add_argument("--TypeEvent", type=str, default="electronGun_pT_0_50", help="Type of event to process")
+parser.add_argument("--InFileName", type=str, default="0", help="Input file name for the simulation")
+the_args = parser.parse_args()
+
+algList = []
+evtsvc = EventDataSvc()
+
+CONSTANTS = {
+}
+
+parseConstants(CONSTANTS)
+
+read = LcioEvent()
+read.OutputLevel = INFO
+read.Files = ["/dataMuC/BIB10TeV/sim_"+the_args.TypeEvent+"/BIB_sim_"+the_args.InFileName+".slcio"]
+algList.append(read)
+
+EventNumber = MarlinProcessorWrapper("EventNumber")
+EventNumber.OutputLevel = INFO
+EventNumber.ProcessorType = "Statusmonitor"
+EventNumber.Parameters = {
+    "HowOften": ["1"]
+}
+
+MyAIDAProcessor = MarlinProcessorWrapper("MyAIDAProcessor")
+MyAIDAProcessor.OutputLevel = INFO
+MyAIDAProcessor.ProcessorType = "AIDAProcessor"
+MyAIDAProcessor.Parameters = {
+    "FileName": ["lctuple_"+the_args.TypeEvent+"_BIB_"+the_args.InFileName],
+    "FileType": ["root"]
+}
+
+Output_REC = MarlinProcessorWrapper("Output_REC")
+Output_REC.OutputLevel = INFO
+Output_REC.ProcessorType = "LCIOOutputProcessor"
+Output_REC.Parameters = {
+    "DropCollectionTypes": ["MCParticle"],
+    "DropCollectionNames": [],
+    "FullSubsetCollections": [],
+    "KeepCollectionNames": [],
+    "LCIOOutputFile": ["/dataMuC/BIB10TeV/reco_"+the_args.TypeEvent+"/BIB_reco_"+the_args.InFileName+".slcio"],
+    "LCIOWriteMode": ["WRITE_NEW"]
+}
+
+InitDD4hep = MarlinProcessorWrapper("InitDD4hep")
+InitDD4hep.OutputLevel = INFO
+InitDD4hep.ProcessorType = "InitializeDD4hep"
+InitDD4hep.Parameters = {
+    "DD4hepXMLFile": ["/code/detector-simulation/geometries/MAIA_v0/MAIA_v0.xml"],
+    "EncodingStringParameterName": ["GlobalTrackerReadoutID"]
+}
+
+VXDBarrelDigitiser = MarlinProcessorWrapper("VXDBarrelDigitiser")
+VXDBarrelDigitiser.OutputLevel = INFO
+VXDBarrelDigitiser.ProcessorType = "DDPlanarDigiProcessor"
+VXDBarrelDigitiser.Parameters = {
+    "CorrectTimesForPropagation": ["true"],
+    "IsStrip": ["false"],
+    "ResolutionT": ["0.03"],
+    "ResolutionU": ["0.005"],
+    "ResolutionV": ["0.005"],
+    "SimTrackHitCollectionName": ["VertexBarrelCollection"],
+    "SimTrkHitRelCollection": ["VBTrackerHitsRelations"],
+    "SubDetectorName": ["Vertex"],
+    "TimeWindowMax": ["0.15"],
+    "TimeWindowMin": ["-0.09"],
+    "TrackerHitCollectionName": ["VBTrackerHits"],
+    "UseTimeWindow": ["true"]
+}
+
+VXDEndcapDigitiser = MarlinProcessorWrapper("VXDEndcapDigitiser")
+VXDEndcapDigitiser.OutputLevel = INFO
+VXDEndcapDigitiser.ProcessorType = "DDPlanarDigiProcessor"
+VXDEndcapDigitiser.Parameters = {
+    "CorrectTimesForPropagation": ["true"],
+    "IsStrip": ["false"],
+    "ResolutionT": ["0.03"],
+    "ResolutionU": ["0.005"],
+    "ResolutionV": ["0.005"],
+    "SimTrackHitCollectionName": ["VertexEndcapCollection"],
+    "SimTrkHitRelCollection": ["VETrackerHitsRelations"],
+    "SubDetectorName": ["Vertex"],
+    "TimeWindowMax": ["0.15"],
+    "TimeWindowMin": ["-0.09"],
+    "TrackerHitCollectionName": ["VETrackerHits"],
+    "UseTimeWindow": ["true"]
+}
+
+InnerPlanarDigiProcessor = MarlinProcessorWrapper("InnerPlanarDigiProcessor")
+InnerPlanarDigiProcessor.OutputLevel = INFO
+InnerPlanarDigiProcessor.ProcessorType = "DDPlanarDigiProcessor"
+InnerPlanarDigiProcessor.Parameters = {
+    "CorrectTimesForPropagation": ["true"],
+    "IsStrip": ["false"],
+    "ResolutionT": ["0.06"],
+    "ResolutionU": ["0.007"],
+    "ResolutionV": ["0.090"],
+    "SimTrackHitCollectionName": ["InnerTrackerBarrelCollection"],
+    "SimTrkHitRelCollection": ["IBTrackerHitsRelations"],
+    "SubDetectorName": ["InnerTrackers"],
+    "TimeWindowMax": ["0.3"],
+    "TimeWindowMin": ["-0.18"],
+    "TrackerHitCollectionName": ["IBTrackerHits"],
+    "UseTimeWindow": ["true"]
+}
+
+InnerEndcapPlanarDigiProcessor = MarlinProcessorWrapper(
+    "InnerEndcapPlanarDigiProcessor")
+InnerEndcapPlanarDigiProcessor.OutputLevel = INFO
+InnerEndcapPlanarDigiProcessor.ProcessorType = "DDPlanarDigiProcessor"
+InnerEndcapPlanarDigiProcessor.Parameters = {
+    "CorrectTimesForPropagation": ["true"],
+    "IsStrip": ["false"],
+    "ResolutionT": ["0.06"],
+    "ResolutionU": ["0.007"],
+    "ResolutionV": ["0.090"],
+    "SimTrackHitCollectionName": ["InnerTrackerEndcapCollection"],
+    "SimTrkHitRelCollection": ["IETrackerHitsRelations"],
+    "SubDetectorName": ["InnerTrackers"],
+    "TimeWindowMax": ["0.3"],
+    "TimeWindowMin": ["-0.18"],
+    "TrackerHitCollectionName": ["IETrackerHits"],
+    "UseTimeWindow": ["true"]
+}
+
+OuterPlanarDigiProcessor = MarlinProcessorWrapper("OuterPlanarDigiProcessor")
+OuterPlanarDigiProcessor.OutputLevel = INFO
+OuterPlanarDigiProcessor.ProcessorType = "DDPlanarDigiProcessor"
+OuterPlanarDigiProcessor.Parameters = {
+    "CorrectTimesForPropagation": ["true"],
+    "IsStrip": ["false"],
+    "ResolutionT": ["0.06"],
+    "ResolutionU": ["0.007"],
+    "ResolutionV": ["0.090"],
+    "SimTrackHitCollectionName": ["OuterTrackerBarrelCollection"],
+    "SimTrkHitRelCollection": ["OBTrackerHitsRelations"],
+    "SubDetectorName": ["OuterTrackers"],
+    "TimeWindowMax": ["0.3"],
+    "TimeWindowMin": ["-0.18"],
+    "TrackerHitCollectionName": ["OBTrackerHits"],
+    "UseTimeWindow": ["true"]
+}
+
+OuterEndcapPlanarDigiProcessor = MarlinProcessorWrapper(
+    "OuterEndcapPlanarDigiProcessor")
+OuterEndcapPlanarDigiProcessor.OutputLevel = INFO
+OuterEndcapPlanarDigiProcessor.ProcessorType = "DDPlanarDigiProcessor"
+OuterEndcapPlanarDigiProcessor.Parameters = {
+    "CorrectTimesForPropagation": ["true"],
+    "IsStrip": ["false"],
+    "ResolutionT": ["0.06"],
+    "ResolutionU": ["0.007"],
+    "ResolutionV": ["0.090"],
+    "SimTrackHitCollectionName": ["OuterTrackerEndcapCollection"],
+    "SimTrkHitRelCollection": ["OETrackerHitsRelations"],
+    "SubDetectorName": ["OuterTrackers"],
+    "TimeWindowMax": ["0.3"],
+    "TimeWindowMin": ["-0.18"],
+    "TrackerHitCollectionName": ["OETrackerHits"],
+    "UseTimeWindow": ["true"]
+}
+
+MyEcalBarrelDigi = MarlinProcessorWrapper("MyEcalBarrelDigi")
+MyEcalBarrelDigi.OutputLevel = INFO
+MyEcalBarrelDigi.ProcessorType = "RealisticCaloDigiSilicon"
+MyEcalBarrelDigi.Parameters = {
+    "CellIDLayerString": ["layer"],
+    "calibration_mip": ["0.0001575"],
+    "inputHitCollections": ["ECalBarrelCollection"],
+    "outputHitCollections": ["EcalBarrelCollectionDigi"],
+    "outputRelationCollections": ["EcalBarrelRelationsSimDigi"],
+    #"threshold": ["0.002"],
+    "threshold": ["5e-05"],
+    "thresholdUnit": ["GeV"],
+    "timingCorrectForPropagation": ["1"],
+    "timingCut": ["1"],
+    "timingResolution": ["0"],
+    "timingWindowMax": ["10."],
+    "timingWindowMin": ["-0.5"],
+    "elec_range_mip": ["15000"]
+}
+
+MyEcalBarrelReco = MarlinProcessorWrapper("MyEcalBarrelReco")
+MyEcalBarrelReco.OutputLevel = INFO
+MyEcalBarrelReco.ProcessorType = "RealisticCaloRecoSilicon"
+MyEcalBarrelReco.Parameters = {
+    "CellIDLayerString": ["layer"],
+    #    "calibration_factorsMipGev": ["0.00641222630095"],
+    "calibration_factorsMipGev": ["0.0066150"], #used for v3
+    #"calibration_factorsMipGev": ["0.00826875"],
+    "calibration_layergroups": ["50"],
+    "inputHitCollections": ["EcalBarrelCollectionDigi"],
+    "inputRelationCollections": ["EcalBarrelRelationsSimDigi"],
+    "outputHitCollections": ["EcalBarrelCollectionRec"],
+    "outputRelationCollections": ["EcalBarrelRelationsSimRec"]
+}
+
+MyEcalEndcapDigi = MarlinProcessorWrapper("MyEcalEndcapDigi")
+MyEcalEndcapDigi.OutputLevel = INFO
+MyEcalEndcapDigi.ProcessorType = "RealisticCaloDigiSilicon"
+MyEcalEndcapDigi.Parameters = {
+    "CellIDLayerString": ["layer"],
+    "calibration_mip": ["0.0001575"],
+    "inputHitCollections": ["ECalEndcapCollection"],
+    "outputHitCollections": ["EcalEndcapCollectionDigi"],
+    "outputRelationCollections": ["EcalEndcapRelationsSimDigi"],
+    #"threshold": ["0.002"],
+    "threshold": ["5e-05"],
+    "thresholdUnit": ["GeV"],
+    "timingCorrectForPropagation": ["1"],
+    "timingCut": ["1"],
+    "timingResolution": ["0"],
+    "timingWindowMax": ["10."],
+    "timingWindowMin": ["-0.5"],
+    "elec_range_mip": ["15000"]
+}
+
+MyEcalEndcapReco = MarlinProcessorWrapper("MyEcalEndcapReco")
+MyEcalEndcapReco.OutputLevel = INFO
+MyEcalEndcapReco.ProcessorType = "RealisticCaloRecoSilicon"
+MyEcalEndcapReco.Parameters = {
+    "CellIDLayerString": ["layer"],
+    #    "calibration_factorsMipGev": ["0.00641222630095"],
+    "calibration_factorsMipGev": ["0.0066150"], #used for v3
+    #"calibration_factorsMipGev": ["0.00826875"],
+    "calibration_layergroups": ["50"],
+    "inputHitCollections": ["EcalEndcapCollectionDigi"],
+    "inputRelationCollections": ["EcalEndcapRelationsSimDigi"],
+    "outputHitCollections": ["EcalEndcapCollectionRec"],
+    "outputRelationCollections": ["EcalEndcapRelationsSimRec"]
+}
+
+MyHcalBarrelDigi = MarlinProcessorWrapper("MyHcalBarrelDigi")
+MyHcalBarrelDigi.OutputLevel = INFO
+MyHcalBarrelDigi.ProcessorType = "RealisticCaloDigiScinPpd"
+MyHcalBarrelDigi.Parameters = {
+    "CellIDLayerString": ["layer"],
+    "calibration_mip": ["0.0004725"],
+    "inputHitCollections": ["HCalBarrelCollection"],
+    "outputHitCollections": ["HcalBarrelCollectionDigi"],
+    "outputRelationCollections": ["HcalBarrelRelationsSimDigi"],
+    "ppd_mipPe": ["15"],
+    "ppd_npix": ["2000"],
+    "ppd_npix_uncert": ["0"],
+    "ppd_pix_spread": ["0"],
+    "threshold": ["0.5"],
+    "thresholdUnit": ["MIP"],
+    "timingCorrectForPropagation": ["1"],
+    "timingCut": ["1"],
+    "timingResolution": ["0"],
+    "timingWindowMax": ["10."],
+    "timingWindowMin": ["-0.5"]
+}
+
+MyHcalBarrelReco = MarlinProcessorWrapper("MyHcalBarrelReco")
+MyHcalBarrelReco.OutputLevel = INFO
+MyHcalBarrelReco.ProcessorType = "RealisticCaloRecoScinPpd"
+MyHcalBarrelReco.Parameters = {
+    "CellIDLayerString": ["layer"],
+    #    "calibration_factorsMipGev": ["0.0287783798145"],
+    "calibration_factorsMipGev": ["0.024625"],
+    "calibration_layergroups": ["100"],
+    "inputHitCollections": ["HcalBarrelCollectionDigi"],
+    "inputRelationCollections": ["HcalBarrelRelationsSimDigi"],
+    "outputHitCollections": ["HcalBarrelCollectionRec"],
+    "outputRelationCollections": ["HcalBarrelRelationsSimRec"],
+    "ppd_mipPe": ["15"],
+    "ppd_npix": ["2000"]
+}
+
+MyHcalEndcapDigi = MarlinProcessorWrapper("MyHcalEndcapDigi")
+MyHcalEndcapDigi.OutputLevel = INFO
+MyHcalEndcapDigi.ProcessorType = "RealisticCaloDigiScinPpd"
+MyHcalEndcapDigi.Parameters = {
+    "CellIDLayerString": ["layer"],
+    "calibration_mip": ["0.0004725"],
+    "inputHitCollections": ["HCalEndcapCollection"],
+    "outputHitCollections": ["HcalEndcapCollectionDigi"],
+    "outputRelationCollections": ["HcalEndcapRelationsSimDigi"],
+    "ppd_mipPe": ["15"],
+    "ppd_npix": ["2000"],
+    "ppd_npix_uncert": ["0"],
+    "ppd_pix_spread": ["0"],
+    "threshold": ["0.5"],
+    "thresholdUnit": ["MIP"],
+    "timingCorrectForPropagation": ["1"],
+    "timingCut": ["1"],
+    "timingResolution": ["0"],
+    "timingWindowMax": ["10."],
+    "timingWindowMin": ["-0.5"]
+}
+
+MyHcalEndcapReco = MarlinProcessorWrapper("MyHcalEndcapReco")
+MyHcalEndcapReco.OutputLevel = INFO
+MyHcalEndcapReco.ProcessorType = "RealisticCaloRecoScinPpd"
+MyHcalEndcapReco.Parameters = {
+    "CellIDLayerString": ["layer"],
+    #   "calibration_factorsMipGev": ["0.0285819096797"],
+    "calibration_factorsMipGev": ["0.024625"],
+    "calibration_layergroups": ["100"],
+    "inputHitCollections": ["HcalEndcapCollectionDigi"],
+    "inputRelationCollections": ["HcalEndcapRelationsSimDigi"],
+    "outputHitCollections": ["HcalEndcapCollectionRec"],
+    "outputRelationCollections": ["HcalEndcapRelationsSimRec"],
+    "ppd_mipPe": ["15"],
+    "ppd_npix": ["2000"]
+}
+
+MyDDSimpleMuonDigi = MarlinProcessorWrapper("MyDDSimpleMuonDigi")
+MyDDSimpleMuonDigi.OutputLevel = INFO
+MyDDSimpleMuonDigi.ProcessorType = "DDSimpleMuonDigi"
+MyDDSimpleMuonDigi.Parameters = {
+    "CalibrMUON": ["70.1"],
+    "MUONCollections": ["YokeBarrelCollection", "YokeEndcapCollection"],
+    "MUONOutputCollection": ["MUON"],
+    "MaxHitEnergyMUON": ["2.0"],
+    "MuonThreshold": ["1e-06"],
+    "RelationOutputCollection": ["RelationMuonHit"]
+}
+
+algList.append(MyAIDAProcessor)
+algList.append(EventNumber)
+algList.append(InitDD4hep)
+algList.append(VXDBarrelDigitiser)
+algList.append(VXDEndcapDigitiser)
+algList.append(InnerPlanarDigiProcessor)
+algList.append(InnerEndcapPlanarDigiProcessor)
+algList.append(OuterPlanarDigiProcessor)
+algList.append(OuterEndcapPlanarDigiProcessor)
+algList.append(MyEcalBarrelDigi)
+algList.append(MyEcalBarrelReco)
+algList.append(MyEcalEndcapDigi)
+algList.append(MyEcalEndcapReco)
+algList.append(MyHcalBarrelDigi)
+algList.append(MyHcalBarrelReco)
+algList.append(MyHcalEndcapDigi)
+algList.append(MyHcalEndcapReco)
+algList.append(MyDDSimpleMuonDigi)
+algList.append(Output_REC)
+
+ApplicationMgr(TopAlg=algList,
+               EvtSel='NONE',
+               EvtMax=-1,
+               ExtSvc=[evtsvc],
+               OutputLevel=INFO
+               )
